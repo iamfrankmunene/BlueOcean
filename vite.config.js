a131a87b6a7111905038f29cf1828a4166534b2b
iamfrankmunene <134502286+iamfrankmunene@users.noreply.github.com>
2023-11-15 22:52:19 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/main.jsx', 
+      },
+    },
+    optimizeDeps: {
+      include: ['src/main.jsx'],
+    },
+  },
+})
